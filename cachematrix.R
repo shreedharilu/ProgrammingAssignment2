77d460d0a81824d375979aefe9c4409a8af0f5d3
shreedharilu <61973273+shreedharilu@users.noreply.github.com>
2020-12-29 14:30:11 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Get mean by using library matlib with inverse function inv()
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Almost same as example in course
+library(matlib)
+makeCacheMatrix <- function(x = matrix())
+  {
+  
+  inv<-NULL
+  set<-function(y)
+  {
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() x
+  setinverse <- function (inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Almost same as example in the course
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv<-inv(data)
+  x$setmean(inv)
+  inv
 }
